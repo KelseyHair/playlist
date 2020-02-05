e7109fcdb05e4e71028e59ec96b5975128c1950a
KelseyHair <Sheliashate@gmail.com>
2020-02-05 23:21:57 +0000

    Keep Going

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var songName = ["The Hero", "Golden Wind", "Clattonia"];
+var songArtist = ["JAM Project", "Hirohiko Araki", "OxT"];
+var songLength = ["1:30", "4:53", "1:29"];
+var imageLink = ["https://i.ytimg.com/vi/atxYe-nOa9w/maxresdefault.jpg","https://vignette.wikia.nocookie.net/jjba/images/f/f8/Overture.png/revision/latest?cb=20181217024652","https://i1.sndcdn.com/artworks-000136539716-raqs3f-t500x500.jpg",];
+var songLink = ["https://www.youtube.com/watch?v=atxYe-nOa9w&t=26s", "https://www.youtube.com/watch?v=2MtOpB5LlUA", "https://www.youtube.com/watch?v=KOWcj7XKnfQ"];
+$("#add").click(function(){
+songArtist.forEach(function(artist){
+$("#artist").append(artists)
+});
+songName.forEach(function(songs){
+$("#songs").append(songs)
+});
+songLength.forEach(function(length){
+});
