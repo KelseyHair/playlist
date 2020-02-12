112105350c1d925338d6cab1c46bae448c472b9d
KelseyHair <Sheliashate@gmail.com>
2020-02-12 23:23:19 +0000

    Almost Done

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
-var songName = ["The Hero", "Golden Wind", "Clattonia"];
-var songArtist = ["JAM Project", "Hirohiko Araki", "OxT"];
-var songLength = ["1:30", "4:53", "1:29"];
-var imageLink = ["https://i.ytimg.com/vi/atxYe-nOa9w/maxresdefault.jpg","https://vignette.wikia.nocookie.net/jjba/images/f/f8/Overture.png/revision/latest?cb=20181217024652","https://i1.sndcdn.com/artworks-000136539716-raqs3f-t500x500.jpg",];
-var songLink = ["https://www.youtube.com/watch?v=atxYe-nOa9w&t=26s", "https://www.youtube.com/watch?v=2MtOpB5LlUA", "https://www.youtube.com/watch?v=KOWcj7XKnfQ"];
+
 $("#add").click(function(){
-songArtist.forEach(function(artist){
-$("#artist").append(artists)
+
+var playlist = [
+    {
+        name: "The Hero",
+        length: 1.30,
+        artist: "JAM Project",
+        songlink: "https://www.youtube.com/watch?v=atxYe-nOa9w&t=26s",
+        imagelink: "https://i.ytimg.com/vi/atxYe-nOa9w/maxresdefault.jpg",
+    },
+    {
+        name: "Golden Wind",
+        length: 4.53,
+        artist: "Hirohiko Araki",
+        songlink: "https://www.youtube.com/watch?v=2MtOpB5LlUA",
+        imagelink: "https://vignette.wikia.nocookie.net/jjba/images/f/f8/Overture.png/revision/latest?cb=20181217024652",
+    },
+    {
+        name: "Clattanoia",
+        length: 1.29,
+        artist: "OxT",
+        songlink: "https://www.youtube.com/watch?v=KOWcj7XKnfQ",
+        imagelink: "https://i1.sndcdn.com/artworks-000136539716-raqs3f-t500x500.jpg",
+    }
+
+]
+
+playlist.forEach(function(song){
+
+$("#form").push
+
+$("#cart").append("<p>" + song["name"] + " " + song["length"] + "</p>");
+
+
+
 });
-songName.forEach(function(songs){
-$("#songs").append(songs)
-});
-songLength.forEach(function(length){
+
 });
